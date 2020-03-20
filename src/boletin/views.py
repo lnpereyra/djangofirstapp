00f7ff8eccd9afa3a922aeb1365690acbd0572a9
lnpereyra <lukas.gd7@gmail.com>
2020-03-20 19:20:11 -0300

    New view and template for home page

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .forms import RegForm
 # Create your views here.
+def home(request):
+    form = RegForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "home.html", context)
