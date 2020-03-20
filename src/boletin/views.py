a819e79a7d22f6548cb77389fc22a7461d5dc191
lnpereyra <lukas.gd7@gmail.com>
2020-03-20 20:05:53 -0300

    Form model modification in admin panel

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
 from .forms import RegForm
+from .models import Registrado
+
 # Create your views here.
 def home(request):
-    form = RegForm()
+    form = RegForm(request.POST or None)
+    if form.is_valid():
+        form_data = form.cleaned_data
+        abc = form_data.get("email")
+        abc2 = form_data.get("nombre")
+        obj = Registrado.objects.create(email=abc, nombre=abc2)
+
     context = {
         "form": form,
     }
